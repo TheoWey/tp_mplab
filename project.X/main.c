c028ee24c977a6fbb45b766857f4da043c1f2df5
TheoWey <wey.theo@outlook.fr>
2025-04-04 14:54:10 +0200

    commit initial

---
+++ project.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: weyth
+ *
+ * Created on 4 avril 2025, 14:53
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ project.X/main.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   main.h
+ * Author: weyth
+ *
+ * Created on 4 avril 2025, 14:53
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MAIN_H */
+
